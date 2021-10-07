e38458bbe20659b93d172cd877f68c165cb8b8d6
Maxim Samsonov <m.samsonov@computer.org>
2021-10-07 11:00:40 +0300

    Pure 'C' test. no 7

--- tests/main.c
+++ tests/main.c
@@ -27,10 +27,10 @@
  * 
  */
 
+#include <tebako-defines.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
-#include <tebako-defines.h>
 #include <tebako-io.h>
 #include "tebako-fs.h"
 #include "tests-defines.h"
