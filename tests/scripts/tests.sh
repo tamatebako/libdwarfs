1cc87d831af2c1830fcc7acdeb11f9ca697c5ace
Maxim Samsonov <m.samsonov@computer.org>
2021-12-11 22:42:56 +0300

    Cache only 2

---
+++ tests/scripts/tests.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+#
+# Copyright (c) 2021, [Ribose Inc](https://www.ribose.com).
+# All rights reserved.
+# This file is a part of tebako
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+# ......................................................................
+#  --  
+test_smoke_test() {
+   echo "==> smoke test"
+   v=0
+   assertEquals 0 $v
+}
+
+
+# ......................................................................
+# main
+
+DIR0="$( cd "$( dirname "$0" )" && pwd )"
+DIR_ROOT="$( cd $DIR0/../.. && pwd )"
+DIR_TESTS="$( cd $DIR_ROOT/tests && pwd )"
+
+echo "Running libdwarfs additional tests"
+. $DIR_TESTS/shunit2/shunit2
