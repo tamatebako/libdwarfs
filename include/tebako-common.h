3d565975663d98b8f22d0fe0be5ce435caf76255
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2022-09-30 16:58:23 +0300

    Fixed is_tebako_path

--- include/tebako-common.h
+++ include/tebako-common.h
@@ -74,7 +74,6 @@
 #define TEBAKO_PATH_LENGTH 2048
 #endif
 
-/* This shall match tebako TEBAKO_MOUNT_POINT definition at CMakeLists.txt */
 #ifdef _WIN32
 #define TEBAKO_MOUNT_POINT "\\\\?\\__tebako_memfs__"
 #define TEBAKO_MOUNT_POINT_W L"\\\\?\\__tebako_memfs__"
