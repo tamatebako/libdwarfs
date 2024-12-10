5d326ac13fb54cc2c97dd59cc59c44613de9d6c2
Maxim Samsonov <m.samsonov@computer.org>
2024-12-10 11:02:45 +0300

    chore: tebako-mfs tests

--- tests/tests-mfs.cpp
+++ tests/tests-mfs.cpp
@@ -1,30 +1,100 @@
-#include "tests.h"
+/**
+ *
+ * Copyright (c) 2021-2024, [Ribose Inc](https://www.ribose.com).
+ * All rights reserved.
+ * This file is a part of tebako
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include <cerrno>
+#include <string>
+
+#include <folly/portability/SysMman.h>
+#include <folly/portability/Unistd.h>
+#include <boost/system/error_code.hpp>
+
+#include <tebako-pch.h>
+#include <tebako-common.h>
+
 #include <tebako-mfs.h>
 
 namespace tebako {
 
-class MfsTests : public ::testing::Test {
- protected:
-  const size_t page_size = sysconf(_SC_PAGESIZE);
-  std::vector<uint8_t> memory;
-  mfs* mfs_instance;
-
-  void SetUp() override
-  {
-    memory.resize(page_size * 2);  // Allocate memory for two pages
-    mfs_instance = new mfs(memory.data(), page_size);
-  }
-
-  void TearDown() override { delete mfs_instance; }
-};
-
-TEST_F(MfsTests, lock_and_release)
+std::error_code mfs::lock(dwarfs::file_off_t offset, size_t size)
 {
-  auto ec = mfs_instance->lock(0, page_size);
-  EXPECT_FALSE(ec) << "Expected no error, but got: " << ec.message();
+  std::error_code ec;
+  auto addr = reinterpret_cast<const uint8_t*>(addr_) + offset;
+  if (::mlock(addr, size) != 0) {
+    ec.assign(errno, boost::system::generic_category());
+  }
+  return ec;
+}
 
-  ec = mfs_instance->release(0, page_size);
-  EXPECT_FALSE(ec) << "Expected no error, but got: " << ec.message();
+std::error_code mfs::release(dwarfs::file_off_t offset, size_t size)
+{
+  std::error_code ec;
+  auto misalign = offset % page_size_;
+
+  offset -= misalign;
+  size += misalign;
+  size -= size % page_size_;
+
+  auto addr = reinterpret_cast<const uint8_t*>(addr_) + offset;
+  if (::munlock(addr, size) != 0) {
+    ec.assign(errno, boost::system::generic_category());
+  }
+  return ec;
+}
+
+std::error_code mfs::release_until(dwarfs::file_off_t offset)
+{
+  std::error_code ec;
+
+  offset -= offset % page_size_;
+
+  if (::munlock(addr_, offset) != 0) {
+    ec.assign(errno, boost::system::generic_category());
+  }
+  return ec;
+}
+
+void const* mfs::addr() const
+{
+  return addr_;
+}
+
+size_t mfs::size() const
+{
+  return size_;
+}
+
+mfs::mfs(const void* addr, size_t size) : size_(size), addr_(addr), page_size_(sysconf(_SC_PAGESIZE)) {}
+
+std::filesystem::path const& mfs::path() const
+{
+  static std::filesystem::path p(TEBAKO_MOUNT_POINT);
+  return p;
 }
 
 }  // namespace tebako
